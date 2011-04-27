b5c4afbc8478c276c66b997116e7a2150e207065
Michael Shin <qwerty2370@yahoo.com>
2011-04-27 12:30:45 -0500

    commit

--- BruteForce/src/Main.java
+++ BruteForce/src/Main.java
@@ -8,14 +8,28 @@ public class Main {
         String a = JOptionPane.showInputDialog("Password:");
         File file = new File(System.getProperty("user.dir") + "\\src\\words");
         BufferedReader reader = new BufferedReader(new FileReader(file));
-
         String pass = "";
         String temp = null;
         while (true) {
 
             temp = reader.readLine();
             if (temp == null) {
-                reader.reset();
+                while (true) {
+                    String lol = "";
+                    for (int x = 0; x < a.length(); x++) {
+                        int k = 0;
+                        while (k < 97 || k > 122) {
+                            k = (int) (Math.random() * 128 + 1);
+                        }
+                        char l = (char) k;
+                        lol += Character.toString(l);
+                    }
+                    System.out.println(lol);
+                    if (lol.equalsIgnoreCase(a)) {
+                        pass = temp;
+                        break;
+                    }
+                }
             }
             if (temp.equalsIgnoreCase(a)) {
                 pass = temp;
